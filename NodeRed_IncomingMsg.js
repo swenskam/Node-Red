90491f0380ea9788813711e6dae9807296ca373e
swenskam <kameron.swenson@newellco.com>
2026-02-02 14:13:33 -0600

    Update NodeRed_IncomingMsg.js

--- NodeRed_IncomingMsg.js
+++ NodeRed_IncomingMsg.js
@@ -2,10 +2,15 @@
 In order to call variables in this code using "this" you must bind this to the outside of the function.
 
 export default {
+    data() {
+        return {
+            variable1: 'value1'
+        }
+    }
     mounted() {
         this.$socket.on('msg-input:' + this.id, function(msg) {
-            if (msg.payload) {
-                alert(`received message: ${msg.payload}`)
+            if (msg.payload === 'some condition') {
+                this.variable1 = 'new value'
             }
         }.bind(this))
     }
