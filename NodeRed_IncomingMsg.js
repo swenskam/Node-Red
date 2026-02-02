2c55c67abbc0c8c8e05c2e811357f8bfec17e61e
swenskam <kameron.swenson@newellco.com>
2026-02-02 14:08:57 -0600

    Create NodeRed_IncomingMsg.js

---
+++ NodeRed_IncomingMsg.js
@@ -0,0 +1,23 @@
+/*This code ran in a ui-template node in node red will take any incoming msg.payload and alert on the dashboard what that message was.
+In order to call variables in this code using "this" you must bind this to the outside of the function.
+
+export default {
+    mounted() {
+        this.$socket.on('msg-input:' + this.id, function(msg) {
+            if (msg.payload) {
+                alert(`received message: ${msg.payload}`)
+            }
+        }.bind(this))
+    }
+}
+
+*/ 
+export default {
+    mounted() {
+        this.$socket.on('msg-input:' + this.id, function(msg) {
+            if (msg.payload) {
+                alert(`received message: ${msg.payload}`)
+            }
+        })
+    }
+}
